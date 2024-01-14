8608bf3a12fa9c76e2a80ab2ff59fcfce8e13095
Marshal ram <marshaljr615@gmailcom>
2024-01-14 20:25:58 +0545

    work_outed

--- script.js
+++ script.js
@@ -44,20 +44,53 @@ function upFlipCards() {
   });
 })();
 
-let timeLeft = 60;
+let countDownActive = false;
+let startButton = document.getElementById("start");
+startButton.addEventListener("click", function go() {
+  if (startButton) {
+    countDownActive = true;
+  }
+});
+let stopButton = document.getElementById("stop");
+let card = document.querySelectorAll(".memory-card");
+stopButton.addEventListener("click", function stopCountdown() {
+  if (countDownActive) {
+    countDownActive = false;
+  }
+  for (i = 0; i < card.length; i++) {
+    cards[i].style.pointerEvents = "none";
+  }
+});
+// let cardss = document.querySelectorAll(".memory-card");
+let resumeButton = document.getElementById("resume");
+resumeButton.addEventListener("click", function startCountdown() {
+  if (!countDownActive) {
+    countDownActive = true;
+  }
+  for (let i = 0; i < cards.length; i++) {
+    cards[i].addEventListener("click", function () {
+      if (!flipCard) {
+        flipCard = true;
+      }
+    });
+  }
+});
+
+let timeLeft = 30;
 let countDown = setInterval(function () {
+  if (!countDownActive) return;
   let minutes = Math.floor(timeLeft / 60);
   let seconds = Math.floor(timeLeft % 60);
 
-  document.getElementById("time").innerHTML =
+  document.getElementById("countDown").innerHTML =
     minutes + "min " + seconds + "sec";
   timeLeft--;
 
   if (timeLeft <= 0) {
     disableCards();
     clearInterval(countDown);
-    document.getElementById("time").innerHTML = "Time's Up!";
-    const cards = document.querySelectorAll(".memory-card");
+    document.getElementById("countDown").innerHTML = "Time's Up!";
+    let cards = document.querySelectorAll(".memory-card");
     for (i = 0; i < cards.length; i++) {
       cards[i].style.pointerEvents = "none";
     }
